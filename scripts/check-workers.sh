357106c3202b9a76b02792e5287dbd7186bb0de4
Hakob <hakob.araqelyan.212@mail.ru>
2026-02-07 22:08:30 +0400

    edit call worcing curl

--- scripts/check-workers.sh
+++ scripts/check-workers.sh
@@ -1,41 +1,17 @@
 
 
 check_container_workers() {
-  status_es=$(curl -s -u "elastic:${ELASTIC_PASSWORD}" http://localhost:9200/_cluster/health | jq -r '.status')
-  status_kibana=$(curl -s http://localhost:5601/api/status | jq -r '.status.overall.level')
-  status_logstash_pipeline=$(curl -s -o /dev/null -w "%{http_code}\n" http://localhost:9600)
-  status_logstash_index=$(curl -s -u elastic:${ELASTIC_PASSWORD} "http://localhost:9200/host-syslog-*/_count" | jq '.count')
   local container="$1"
   local status_working="$2"
   
+  status_es=$(curl "$status_working")
 
-    echo "status working container: $status_working"
+  echo "status working container: $status_working"
 
-
-  case "$container" in
-    elasticsearch)
-      if [[ "$status_working" == "$status_es" ]]; then
-        echo "Elasticsearch is ready with status: ${status_working}"
-        return 0
-      fi
-      ;;
-    kibana)
-      if [[ "$status_working" == "$status_kibana" ]]; then
-        echo "Kibana is ready with status: ${status_working}"
-        return 0
-      fi
-      ;;
-    logstash)
-      if [[ "$status_working" == "$status_logstash_pipeline" || "$status_working" == "$status_logstash_index" ]]; then
-        echo "Logstash is ready with status: ${status_working}"
-        return 0
-      fi
-      fi
-      ;;
-    *)
-      echo "Unknown container: $container"
-      ;;
-  esac
-
+  if [[ "$status_working" ]]; then
+    echo "container is ready with status: ${status_working}"
+    return 0
+  fi
+  
   return 1
 }
