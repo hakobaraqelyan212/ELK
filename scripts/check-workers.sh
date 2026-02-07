535ae899219f539c846834428dc2417d2230cee2
Hakob <hakob.araqelyan.212@mail.ru>
2026-02-07 22:43:06 +0400

    edit call worcing curl

--- scripts/check-workers.sh
+++ scripts/check-workers.sh
@@ -4,11 +4,11 @@ check_container_workers() {
   local container="$1"
   local status_working="$2"
   
-  status_es=$(curl "$status_working")
+  status=$(curl "$status_working")
 
   echo "status working container: $status_working"
 
-  if [[ "$status_working" ]]; then
+  if [[ "$status" ]]; then
     echo "container is ready with status: ${status_working}"
     return 0
   fi
